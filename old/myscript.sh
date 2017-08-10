0fd5d2e1984daf8464dc5b18b08943992f549bfe
sharmishtaa <shtaaa@gmail.com>
2017-08-10 11:22:35 -0700

    commands

---
+++ old/myscript.sh
@@ -0,0 +1,5 @@
+echo "I am triggered!" > /home/sharmishtaas/triggeredoutput
+#python /pipeline/luigi-scripts/runme_acquisition.py
+#python /pipeline/luigi-scripts/myscript.py
+#echo "I am triggered!" > /home/sharmishtaas/triggeredoutput1
+
